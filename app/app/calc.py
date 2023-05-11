74b25abb699caf325d340fca41aa01414d159e1b
Kevin Pham <phamtkevin@gmail.com>
2023-05-10 21:31:47 -0700

    TDD with django

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return result"""
+    return x + y
+
+# Test Driven Development - create test cases first then write the program to pass the test case
+def subtract(x, y):
+    """Subtract x from y and return result"""
+    return y - x
