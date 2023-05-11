a0be2a0eaf50022a2f9b4945023b514ebdbb7cbe
Kevin Pham <phamtkevin@gmail.com>
2023-05-10 23:13:10 -0700

    configured docker compose and checks to wait for database

--- app/app/calc.py
+++ app/app/calc.py
@@ -7,7 +7,7 @@ def add(x, y):
     """Add x and y and return result"""
     return x + y
 
-# Test Driven Development - create test cases first then write the program to pass the test case
+
 def subtract(x, y):
     """Subtract x from y and return result"""
     return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -9,13 +9,12 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     """Test calc module"""
-    
+
     def test_add_numbers(self):
         """Test adding numbers together"""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-
 
     def test_subtract_numbesr(self):
         """Test subtracting numbers"""
